7445d1874035b1fa01570d870c455b7cc4bdfc66
Sebastian <sebastiansloma@gmail.com>
2023-07-10 11:43:19 +0200

    added header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,9 @@ import React from 'react'
 function Header() {
   return (
     <heder>
-        <h1>The<strong>Anime</strong>Database</h1>
+        <h1>The
+          <strong>Anime</strong>
+          Database</h1>
     </heder>
   )
 }

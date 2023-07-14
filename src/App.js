afdfd12cc22ab2586cfce59470c4d65412fd5a73
Sebastian <sebastiansloma@gmail.com>
2023-07-14 13:34:25 +0200

    added sidebar.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import Header from "./components/Header";
+import { useState, useEffect } from 'react';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
 
 function App() {
-  return (
-    <div className="App">
-     <Header/>
-    </div>
-  );
+	const [animeList, SetAnimeList] = useState([]);
+	const [topAnime, SetTopAnime] = useState([]);
+	const [search, SetSearch] = useState('');
+	return (
+		<div className='App'>
+			<Header />
+      <div className='content-wrap'>
+        <Sidebar
+        topAnime={topAnime}
+        />
+
+      </div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Sidebar() {
+	return (
+		<aside>
+			<nav>
+				<h3>Top Anime</h3>
+				<a href='#' target='_blank' rel='noreferrer'>Attack on Titan</a>
+				<a href='#' target='_blank' rel='noreferrer'>One Punch Man</a>
+				<a href='#' target='_blank' rel='noreferrer'>Sword Art Online</a>
+			</nav>
+		</aside>
+	);
+}
+
+export default Sidebar;

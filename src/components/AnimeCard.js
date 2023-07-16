099182e104283f6195d4393a10bf6cdac3ae37f6
Sebastian <sebastiansloma@gmail.com>
2023-07-16 13:27:01 +0200

    .

--- src/components/AnimeCard.js
+++ src/components/AnimeCard.js
@@ -1,23 +1,17 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
-import React from 'react'
+import React from 'react';
 
-function AnimeCard({anime}) {
-  return (
-    <article className="anime-card">
-			<a 
-				href={anime.url} 
-				target="_blank" 
-				rel="noreferrer">
+function AnimeCard({ anime }) {
+	return (
+		<article className='anime-card'>
+			<a href={anime.url} target='_blank' rel='noreferrer'>
 				<figure>
-					<img 
-						src={anime.image_url} 
-						alt="Anime Image" />
+					<img src={anime.image_url} alt='Anime Image' />
 				</figure>
-				<h3>{ anime.title }</h3>
+				<h3>{anime.title}</h3>
 			</a>
 		</article>
-	)
-  
+	);
 }
 
-export default AnimeCard
+export default AnimeCard;
